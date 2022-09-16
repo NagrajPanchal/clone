c17df031cc5afa666002e26029fa060dbd607d24
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-16 12:47:01 +0530

    Refactor : PRime Number or Not

--- src/com/bridgelabz/Prime_Not.java
+++ src/com/bridgelabz/Prime_Not.java
@@ -1,2 +1,26 @@
-package com.bridgelabz;public class Prime_Not {
+package com.bridgelabz;
+import java.util.Scanner;
+public class Prime_Not
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int num = scanner.nextInt();
+        int count =0;
+        for(int i=1; i<=num; i++)
+        {
+            if(num%i==0)
+            {
+                count++;
+            }
+        }
+        if(count==2)
+        {
+            System.out.println("Prime");
+        }
+        else {
+            System.out.println("Not Prime");
+        }
+    }
 }
